fd86490603ca2b06097818b3c2e692820c11c4ad
sivaramaaa <sivaramaaa@gmail.com>
2017-02-20 12:21:05 +0530

    Add files via upload

---
+++ encode.js
@@ -0,0 +1,10 @@
+
+str = "http://pastebin.com/7XaXf6wF"
+
+str = str.replace(/http:/g, "^^^"); 
+str = str.replace(/bin/g, "*^$#!")
+str= str.replace(/com/g, "*%=_()");
+str= str.replace(/paste/g, "~~@;;");
+
+
+
